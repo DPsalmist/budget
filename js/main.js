0d7c400cd1c14afc7e8d8fd76bec98fab10adcd6
sodbridgeacademy <sodbridgeacademy@gmail.com>
2024-11-05 13:34:59 +0100

    updated products section

--- js/main.js
+++ js/main.js
@@ -58,3 +58,78 @@
     
 })(jQuery);
 
+
+// Extras
+let currentImageIndex = 0;
+const images = [
+    'img/house1.png',
+    'img/house2.png',
+    // Add more images as needed
+];
+
+function showPortfolioModal(src) {
+    currentImageIndex = images.indexOf(src);
+    document.getElementById("modalPortfolioImage").src = src;
+    document.getElementById("portfolioModal").style.display = "flex"; // Use flex to center
+}
+
+function closePortfolioModal() {
+    document.getElementById("portfolioModal").style.display = "none";
+}
+
+function changeImage(direction) {
+    currentImageIndex += direction;
+
+    // Loop back to the start or end of the images array
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1; // Go to last image
+    } else if (currentImageIndex >= images.length) {
+        currentImageIndex = 0; // Go to first image
+    }
+
+    // Update the modal image
+    document.getElementById("modalPortfolioImage").src = images[currentImageIndex];
+}
+
+
+// Product Arrows
+let currentProductImageIndex = 0;
+const productImages = [
+    'img/productdemo.png', // Image 1
+    'img/productdemo.png', // Image 2
+    'img/productdemo.png', // Image 3
+    'img/productdemo.png',
+    'img/productdemo.png',
+    'img/productdemo.png'
+];
+
+function showProductImageModal(src) {
+    currentProductImageIndex = productImages.indexOf(src);
+    document.getElementById("modalImage").src = src;
+    document.getElementById("imageModal").style.display = "flex";
+}
+
+function closeImageModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+// Close modal when clicking outside image
+document.getElementById("imageModal").addEventListener("click", function(event) {
+    if (event.target === this) {
+        closeImageModal();
+    }
+});
+
+function changeProductImage(direction) {
+    currentProductImageIndex += direction;
+
+    // Wrap around the image list
+    if (currentProductImageIndex < 0) {
+        currentProductImageIndex = productImages.length - 1;
+    } else if (currentProductImageIndex >= productImages.length) {
+        currentProductImageIndex = 0;
+    }
+
+    // Update the modal image
+    document.getElementById("modalImage").src = productImages[currentProductImageIndex];
+}
